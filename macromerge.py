cda46bb75c2209710b14110bc0931bc726d7339b
Geremia Massarelli <geremiam@users.noreply.github.com>
2021-10-31 18:36:10 -0400

    Command line arguments modified. Macros with arguments are properly accounted for.

--- macromerge.py
+++ macromerge.py
@@ -1,5 +1,6 @@
 import argparse
 import re
+import collections
 
 
 if __name__ == "__main__":
@@ -7,10 +8,10 @@ if __name__ == "__main__":
     parser.prog = "merge.py"
     parser.description = "Merge sty files."
     
-    parser.add_argument("-i", "--inputfile", help="Input file", action='append', required=True, type=str)
-    parser.add_argument("outputname", help="Name for output", type=str)
-    #parser.add_argument("--Wannierbands", action="store_true", help="Plot Wannier bands")
-    #parser.add_argument("--Wannierpol", action="store_true", help="Calculate Wannier polarization")
+    parser.add_argument("inputfile", help="Input file", nargs='+', type=str)
+    parser.add_argument("-o", "--outputname", help="Name for output", required=True, type=str)
+    parser.add_argument("-v", "--verbose", help="Show merged macros", action="store_true")
+    #parser.add_argument("-q", "--quiet", help="", action="store_true")
     args = parser.parse_args()
     
     newcommands = []
@@ -18,18 +19,17 @@ if __name__ == "__main__":
     
     com1_re = r'(\\newcommand)'
     com2_re = r'(\\renewcommand)'
-    name_re = r'[ {]*(\\[^ {}\\]*)'
+    name_re = r'[ {]*(\\[^ \[\]{}\\]*)'
     
-    #p = re.compile(r'(\\newcommand)[ {]*(\\[^ {}\\]*)')
     p1 = re.compile(com1_re + name_re)
     p2 = re.compile(com2_re + name_re)
     
     for filename in args.inputfile:
-        # May be important: are the objects unpacked from 'list' actual copies?
         with open(filename, 'r') as fh:
             all_lines = fh.readlines()
-    
+        
         for line in all_lines:
+            line += r' % ' + filename
             m1 = p1.search(line)
             m2 = p2.search(line)
         
@@ -41,9 +41,16 @@ if __name__ == "__main__":
                 assert m1 is None
                 renewcommands += [(m2.group(1), m2.group(2), line)]
     
-    #newcommands.sort(key=lambda x: x[1])
+    # Make list of all commands, with 'newcommand' definitions coming last
+    allcommands = newcommands + renewcommands
+    # Sort list in place. Sorting algorithm is stable, so secondary order is preserved
+    allcommands.sort(key=lambda x: x[1])
     
-    print(newcommands)
+    if args.verbose:
+        for i in allcommands: print(i)
+    
+    names = [el[1] for el in allcommands]
+    print(names)
     print()
-    print(renewcommands)
-    
+    counter = collections.Counter(names)
+    print(counter)
